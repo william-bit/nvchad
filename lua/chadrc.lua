a8ea59d5d02bdf85314ae97b30aa0034afb241dd
William Kc <williamkohinc@gmail.com>
2025-01-23 13:37:52 +0700

    add new option in dashboard

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -31,10 +31,10 @@ M.nvdash = {
   },
 
   buttons = {
-    { txt = "  Find File", keys = "c-p", cmd = "FzfLua files" },
-    { txt = "󰈭  Find Word", keys = "c-f", cmd = "FzfLua live_grep" },
-    { txt = "  Git Files", keys = "c-t", cmd = "FzfLua git_status" },
-    { txt = "  Recent Files", keys = "c-x", cmd = "FzfLua oldfiles" },
+    { txt = "  Find File", keys = "C-p", cmd = "FzfLua files" },
+    { txt = "󰈭  Find Word", keys = "C-f", cmd = "FzfLua live_grep" },
+    { txt = "  Git Files", keys = "C-t", cmd = "FzfLua git_status" },
+    { txt = "  Recent Files", keys = "C-x", cmd = "FzfLua oldfiles" },
     { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
     { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
     { txt = "󰒲  Lazy", keys = "l", cmd = ":Lazy" },
