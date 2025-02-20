83987d1cfdd6d704df901c9e22bd36ac2185be52
William Kc <williamkohinc@gmail.com>
2025-02-20 11:47:52 +0700

    refractor code chadrc keystorke to config.keystroke

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,112 +5,6 @@
 ---@type ChadrcConfig
 local M = {}
 
-local endcommand = {
-  "h",
-  "j",
-  "k",
-  "l",
-  "H",
-  "M",
-  "L",
-  "e",
-  "q",
-  "E",
-  "w",
-  "W",
-  "b",
-  "B",
-  "%",
-  "n",
-  "^",
-  "*",
-  "_",
-  "G",
-  "+",
-  "=",
-  ":",
-  "_",
-  "-",
-  ";",
-  ",",
-  "gg",
-  "gd",
-  "gD",
-  "dd",
-  "yy",
-  "aw",
-  "iw",
-  "aW",
-  "iW",
-  "as",
-  "is",
-  "ap",
-  "ip",
-  "a]",
-  "a[",
-  "i]",
-  "i[",
-  "a)",
-  "a(",
-  "ab",
-  "i)",
-  "i(",
-  "ib",
-  "a>",
-  "a<",
-  "i>",
-  "i<",
-  "at",
-  "it",
-  "a}",
-  "a{",
-  "aB",
-  "i}",
-  "i{",
-  "iB",
-  'a"',
-  "a'",
-  "a`",
-  'i"',
-  "i'",
-  "i`",
-}
-
-local function contains(list, x)
-  for _, v in ipairs(list) do
-    if v == x then
-      return true
-    end
-  end
-  return false
-end
-local array_key = { "", "" }
-vim.on_key(function(_, key)
-  if key and #key > 0 then
-    local mode = vim.api.nvim_get_mode()
-    if mode.mode == "n" or mode.mode == "no" then
-      local input_key = table.concat(array_key)
-      local typed = vim.fn.keytrans(key)
-      local last_typed = array_key[#array_key]
-      local last_2typed = array_key[#array_key - 1] .. array_key[#array_key]
-      if
-        input_key == "0"
-        or input_key == "$"
-        or string.match(typed, "<.+>")
-        or string.match(last_typed, "<.+>")
-        or string.match(last_2typed, "[f|F|t|T].")
-        or string.match(last_2typed, "[a-zA-Z][0|$]")
-        or contains(endcommand, last_typed)
-        or contains(endcommand, last_2typed)
-      then
-        array_key = { "", "" }
-      end
-      table.insert(array_key, typed)
-    end
-    vim.cmd [[ doautoall ]]
-  end
-end)
-
 M.base46 = {
   theme = "gruvbox",
 
@@ -218,7 +112,7 @@ M.ui = {
         return "Recording @" .. reg .. " "
       end,
       feedkey = function()
-        return "[" .. table.concat(array_key) .. "] "
+        return "[" .. table.concat(require("configs.keystroke").array_key) .. "] "
       end,
     },
   },
