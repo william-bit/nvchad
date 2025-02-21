91b8f5294d3a39bf359cd4beb60434760495dbdb
William Kc <williamkohinc@gmail.com>
2025-02-21 17:19:15 +0700

    remove duplicate mapping in fzflua and lsp attach

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -13,78 +13,85 @@ return {
       { "<c-p>", "<cmd>FzfLua files<CR>" },
       { "<c-t>", "<cmd>FzfLua git_status<CR>" },
       { "<c-x>", "<cmd>FzfLua oldfiles<CR>" },
-      { "<leader>fh", "<cmd>FzfLua help_tags<CR>" },
-      { "gD", "<cmd>FzfLua lsp_declarations<CR>", desc = "Go to declaration" },
-      { "gd", "<cmd>FzfLua lsp_definitions<CR>", desc = "Go to definition" },
-      { "gi", "<cmd>FzfLua lsp_implementations<CR>", desc = "Go to implementation" },
-      { "<leader>D", "<cmd>FzfLua lsp_typedefs<CR>", desc = "Go to type definition" },
-      { "gr", "<cmd>FzfLua lsp_references<CR>", desc = "Go to references" },
-      {
-        "g.",
-        function()
-          require("fzf-lua").lsp_code_actions {
-            winopts = {
-              relative = "cursor",
-              width = 0.6,
-              height = 0.6,
-              row = 1,
-              preview = { vertical = "up:70%" },
-            },
-          }
-        end,
-        desc = "LSP code action",
-        mode = { "n", "v" },
-      },
-      { "<leader>sh", vim.lsp.buf.signature_help, desc = "Show signature help" },
-      { "<leader>wa", vim.lsp.buf.add_workspace_folder, desc = "Add workspace folder" },
-      { "<leader>wr", vim.lsp.buf.remove_workspace_folder, desc = "Remove workspace folder" },
-      {
-        "<leader>wl",
-        function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end,
-        desc = "List workspace folders",
-      },
+      { "gD", "<cmd>FzfLua lsp_declarations<CR>", desc = "Go to declaration", mode = { "n" } },
+      { "gd", "<cmd>FzfLua lsp_definitions<CR>", desc = "Go to definition", mode = { "n" } },
+      { "gi", "<cmd>FzfLua lsp_implementations<CR>", desc = "Go to implementation", mode = { "n" } },
+      { "gr", "<cmd>FzfLua lsp_references<CR>", desc = "Go to references", mode = { "n" } },
+      { "g.", "<cmd>FzfLua lsp_code_actions<CR>", desc = "LSP code action", mode = { "n" } },
+      { "<leader>D", "<cmd>FzfLua lsp_typedefs<CR>", desc = "Go to type definition", mode = { "n" } },
+      { "<leader>fh", "<cmd>FzfLua help_tags<CR>", mode = { "n" } },
     },
-    opts = {
-      keymap = {
-        fzf = {
-          -- Send finding to quick fix like telescope
-          ["ctrl-q"] = "select-all+accept",
+    opts = function()
+      -- use `fzf-lua` for replace vim.ui.select
+      require("fzf-lua").register_ui_select()
+
+      return {
+        keymap = {
+          fzf = {
+            -- Send finding to quick fix like telescope
+            ["ctrl-q"] = "select-all+accept",
+          },
         },
-      },
-      oldfiles = {
-        color_icons = false,
-      },
-      files = {
-        previewer = false,
-        color_icons = false,
-        path_shorten = false,
-        multiprocess = true,
-      },
-      grep = {
-        color_icons = false,
-        path_shorten = true,
-        multiprocess = true,
-      },
-      buffers = {
-        color_icons = false,
-        path_shorten = true,
-      },
-      git = {
+        oldfiles = {
+          color_icons = false,
+          stat_file = true, -- verify files exist on disk
+        },
         files = {
-          color_icons = false,
-        },
-        status = {
-          color_icons = false,
-        },
-      },
-      lsp = {
-        color_icons = false,
-        code_actions = {
           previewer = false,
+          color_icons = false,
+          path_shorten = false,
+          multiprocess = true,
         },
-      },
-    },
+        grep = {
+          color_icons = false,
+          path_shorten = true,
+          multiprocess = true,
+        },
+        buffers = {
+          color_icons = false,
+          path_shorten = true,
+        },
+        git = {
+          files = {
+            color_icons = false,
+          },
+          status = {
+            color_icons = false,
+          },
+        },
+        lsp = {
+          color_icons = false,
+          code_actions = {
+            previewer = false,
+          },
+        },
+        ui_select = function(fzf_opts, items)
+          return vim.tbl_deep_extend("force", fzf_opts, {
+            prompt = " ",
+            winopts = {
+              title = " " .. vim.trim((fzf_opts.prompt or "Select"):gsub("%s*:%s*$", "")) .. " ",
+              title_pos = "center",
+            },
+          }, fzf_opts.kind == "codeaction" and {
+            winopts = {
+              layout = "vertical",
+              -- height is number of items minus 15 lines for the preview, with a max of 80% screen height
+              height = math.floor(math.min(vim.o.lines * 0.8 - 16, #items + 2) + 0.5) + 16,
+              width = 0.5,
+              preview = {
+                layout = "vertical",
+                vertical = "down:15,border-top",
+              },
+            },
+          } or {
+            winopts = {
+              width = 0.5,
+              -- height is number of items, with a max of 80% screen height
+              height = math.floor(math.min(vim.o.lines * 0.8, #items + 2) + 0.5),
+            },
+          })
+        end,
+      }
+    end,
   },
 }
