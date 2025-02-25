22383ef8e44f3ec31c82a6519b1380cd5d9fe5b7
William Kc <williamkohinc@gmail.com>
2025-02-25 15:42:10 +0700

    move nvim-dap to single file

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -16,30 +16,6 @@ local function extend_or_override(config, custom, ...)
 end
 
 return {
-  -- Ensure java debugger and test packages are installed.
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      -- Simple configuration to attach to remote java debug process
-      -- Taken directly from https://github.com/mfussenegger/nvim-dap/wiki/Java
-      local dap = require "dap"
-      dap.configurations.java = {
-        {
-          type = "java",
-          request = "attach",
-          name = "Debug (Attach) - Remote",
-          hostName = "127.0.0.1",
-          port = 5005,
-        },
-      }
-    end,
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = { ensure_installed = { "java-debug-adapter", "java-test" } },
-      },
-    },
-  },
   -- Set up nvim-jdtls to attach to java files.
   {
     "mfussenegger/nvim-jdtls",
